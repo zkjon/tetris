daaa30bbd6e3ba8086960c4d0af2de213509f2ae
zkj0n <focusyts@gmail.com>
2025-04-15 03:08:36 +0200

    implement game loop and collision detection for Tetris pieces

--- src/main.js
+++ src/main.js
@@ -35,7 +35,7 @@ const map = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+  [1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
 ];
 // for (let i = 0; i < MAP_HEIGHT; i++) {
 //     map[i] = []
@@ -45,10 +45,35 @@ const map = [
 // }
 
 // 2. game update
-function loop() {
+let countDown = 0;
+let previousTime = 0;
+let interval = 1000; // 1 second
+function loop(time = 0) {
+  let thisTime = time - previousTime;
+
+  countDown += thisTime;
+
+  if (countDown > interval) {
+    piece.position.y++;
+    if (checkCollision()) {
+      piece.position.y--;
+      fixPiece();
+      clearLines();
+    }
+  }
+
+  countDown = 0
+
   print();
   window.requestAnimationFrame(loop);
 }
+
+// 4. create piece
+const piece = {
+  position: { x: 5, y: 5 },
+  shape: PIECES[0],
+};
+
 // 2.1. print map
 function print() {
   context.fillStyle = "#e0e0e0";
@@ -69,15 +94,6 @@ function print() {
     });
   });
 
-  // 2.3. draw a pieces
-  const piece= {
-    position: {
-      x: 5,
-      y: 5,
-    },
-    shape: PIECES[0],
-  }
-
   piece.shape.forEach((row, y) => {
     row.forEach((cell, x) => {
       if (cell) {
@@ -91,5 +107,66 @@ function print() {
     });
   });
 }
+
+// 6. check collision
+function checkCollision() {
+  return piece.shape.find((row, y) => {
+    return row.find((cell, x) => {
+      return (
+        cell !== 0 && // if the cell is not empty
+        map[y + piece.position.y]?.[x + piece.position.x] !== 0
+      ); // if the cell is not out of bounds
+    });
+  });
+}
+
+// 7. fix piece
+function fixPiece() {
+  piece.shape.forEach((row, y) => {
+    row.forEach((cell, x) => {
+      if (cell !== 0) {
+        map[y + piece.position.y][x + piece.position.x] = cell;
+      }
+    });
+  });
+
+  piece.position.y = 0;
+  piece.position.x = 0;
+}
+
+// 8. clear lines
+function clearLines() {
+  map.forEach((row, y) => {
+    if (row.every((cell) => cell !== 0)) {
+      map.splice(y, 1); // remove the row
+      map.unshift(Array(MAP_WIDTH).fill(0)); // add a new row at the top
+    }
+  });
+}
+
+// 5. keydown event
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowLeft") {
+    piece.position.x--;
+    if (checkCollision()) {
+      piece.position.x++;
+    }
+  }
+  if (event.key === "ArrowRight") {
+    piece.position.x++;
+    if (checkCollision()) {
+      piece.position.x--;
+    }
+  }
+
+  if (event.key === "ArrowDown") {
+    piece.position.y++;
+    if (checkCollision()) {
+      piece.position.y--;
+      fixPiece();
+      clearLines();
+    }
+  }
+});
 
 loop();
